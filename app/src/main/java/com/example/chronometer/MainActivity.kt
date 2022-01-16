5ffd82e2872ac4bf4d8a0cde3ed44333ed8d5a19
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2022-01-16 15:10:27 +0200

    add dimens, final result is a when user click on a button app return seconds, which passed from when last clicked was made or app will starts

--- app/src/main/java/com/example/chronometer/MainActivity.kt
+++ app/src/main/java/com/example/chronometer/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.chronometer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.example.chronometer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val chronometer = SystemClock.elapsedRealtime()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+       binding.start.setOnClickListener {
+           updateButton()
+       }
+    }
+
+    fun updateButton() {
+        val nowSeconds = SystemClock.elapsedRealtime()
+        val chronometerResult = getString(R.string.chronometer_text,
+            (nowSeconds - chronometer) /1000)
+        binding.chronometer.text = chronometerResult
     }
 }
