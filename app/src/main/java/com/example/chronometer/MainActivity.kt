7c11eb45ad4a182257a2667f0cfea7c5a22bd071
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2022-01-16 17:07:25 +0200

    update our chronometer added startTimer(), stopTimer()

--- app/src/main/java/com/example/chronometer/MainActivity.kt
+++ app/src/main/java/com/example/chronometer/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.chronometer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.widget.Chronometer
 import com.example.chronometer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val chronometer = SystemClock.elapsedRealtime()
+    private lateinit var chronometer: Chronometer
+    private val elapsedTime = SystemClock.elapsedRealtime()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,15 +17,28 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-       binding.start.setOnClickListener {
-           updateButton()
-       }
+
+        binding.startBtn.setOnClickListener {
+            startTimer()
+        }
+
+        binding.stopBtn.setOnClickListener {
+            stopTimer()
+        }
     }
 
-    fun updateButton() {
-        val nowSeconds = SystemClock.elapsedRealtime()
-        val chronometerResult = getString(R.string.chronometer_text,
-            (nowSeconds - chronometer) /1000)
-        binding.chronometer.text = chronometerResult
+    private fun startTimer() {
+        chronometer = binding.chronometer
+        chronometer.start()
+
+        binding.startBtn.isEnabled = false
+        binding.stopBtn.isEnabled = true
+    }
+
+    private fun stopTimer() {
+        chronometer.stop()
+
+        binding.stopBtn.isEnabled = false
+        binding.startBtn.isEnabled = true
     }
 }
